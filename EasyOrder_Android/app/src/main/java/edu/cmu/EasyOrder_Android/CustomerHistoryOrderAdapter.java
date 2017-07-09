365c8b7886028e1d65d0f9bb766f9c8a5e4de97c
louis-xu-ustc <yunpengx@andrew.cmu.edu>
2017-07-09 16:24:18 -0400

    [yunpengx] add profile for retailer and customer

---
+++ EasyOrder_Android/app/src/main/java/edu/cmu/EasyOrder_Android/CustomerHistoryOrderAdapter.java
@@ -0,0 +1,48 @@
+package edu.cmu.EasyOrder_Android;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yunpengx on 7/8/17.
+ */
+
+public class CustomerHistoryOrderAdapter extends ArrayAdapter<Dish> {
+    ArrayList<Dish> dishArrayList = new ArrayList<>();
+
+    public CustomerHistoryOrderAdapter(Context context, int textViewResourceId, ArrayList<Dish> objects) {
+        super(context, textViewResourceId, objects);
+        dishArrayList = objects;
+    }
+
+    @Override
+    public int getCount() {
+        return super.getCount();
+    }
+
+    @Override
+    public View getView(int pos, View convertView, ViewGroup parent) {
+        View v = convertView;
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        v = inflater.inflate(R.layout.customer_history_order_view, null);
+        Dish dish = dishArrayList.get(pos);
+
+        ImageView imageView = (ImageView) v.findViewById(R.id.history_order_dish_image);
+        imageView.setImageResource(R.drawable.default_dish_icon);
+        TextView dishName = (TextView) v.findViewById(R.id.history_order_dish_name);
+        dishName.setText(dish.getName());
+        TextView dishPrice = (TextView) v.findViewById(R.id.history_order_dish_price);
+        dishPrice.setText(new StringBuilder().append("$ ").append(String.valueOf(dish.getPrice())).toString());
+        TextView dishQuantity = (TextView) v.findViewById(R.id.history_order_dish_quantity);
+        dishQuantity.setText(String.valueOf(dish.getQuantity()));
+
+        return v;
+    }
+}
